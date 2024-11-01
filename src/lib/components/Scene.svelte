2e8a0ca7c8e714bbdd6e57e8904f282639937107
Ed Sunman <ed.sunman@freshlabel.co.uk>
2024-11-01 20:56:08 +0000

    First Commit!

---
+++ src/lib/components/Scene.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { T, useTask } from '@threlte/core';
+	import {
+		Grid,
+		OrbitControls,
+		InstancedMesh,
+		Instance,
+		useGltf,
+		interactivity
+	} from '@threlte/extras';
+</script>
+
+<Grid
+	name="debug"
+	gridSize={[50, 50]}
+	cellColor={'#46536b'}
+	sectionColor="#ffffff"
+	sectionThickness={0}
+	fadeDistance={50}
+	position.y={-0.01}
+/>
+
+<T.PerspectiveCamera makeDefault name="dev camera" position={[20, 20, 20]} fov={15}>
+	<OrbitControls />
+</T.PerspectiveCamera>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import App from '../lib/components/App.svelte';
+	//import '../app.css';
+</script>
+
+<div>
+	<App />
+</div>
+
+<style>
+	:global(body) {
+		margin: 0;
+		font-family: 'Share Tech', sans-serif;
+		font-weight: 400;
+		font-style: normal;
+	}
+
+	div {
+		width: 100vw;
+		height: 100vh;
+		background: rgb(0, 0, 0);
+	}
+</style>
